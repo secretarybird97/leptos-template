cb3199007a938fc094ded4955fff86590e1cc7ee
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2024-02-01 19:12:20 -0800

    Added leptosfmt and daisyui

--- src/app.rs
+++ src/app.rs
@@ -9,10 +9,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/visual-start.css"/>
 
         // sets the document title
@@ -22,10 +18,7 @@ pub fn App() -> impl IntoView {
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
-            view! {
-                <ErrorTemplate outside_errors/>
-            }
-            .into_view()
+            view! { <ErrorTemplate outside_errors/> }.into_view()
         }>
             <main>
                 <Routes>
@@ -44,14 +37,13 @@ fn HomePage() -> impl IntoView {
         <Title text="Leptos + Tailwindcss"/>
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
+
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                    <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    <button on:click=move |_| set_value.update(|value| *value += 1) class="btn">
                         "+"
                     </button>
-                    <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
-                        {value}
-                    </button>
-                    <button on:click=move |_| set_value.update(|value| *value -= 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    <button class="btn">{value}</button>
+                    <button on:click=move |_| set_value.update(|value| *value -= 1) class="btn">
                         "-"
                     </button>
                 </div>
