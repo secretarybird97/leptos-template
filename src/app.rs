9bb67f262cfd531409a6864a52263753fef890e2
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2024-02-01 20:06:16 -0800

    Navbar implementation daisyUI

--- src/app.rs
+++ src/app.rs
@@ -37,7 +37,7 @@ fn HomePage() -> impl IntoView {
         <Title text="Leptos + Tailwindcss"/>
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
-
+                <Navbar/>
                 <div class="flex flex-row-reverse flex-wrap m-auto">
                     <button on:click=move |_| set_value.update(|value| *value += 1) class="btn">
                         "+"
@@ -45,6 +45,56 @@ fn HomePage() -> impl IntoView {
                     <button class="btn">{value}</button>
                     <button on:click=move |_| set_value.update(|value| *value -= 1) class="btn">
                         "-"
+                    </button>
+                </div>
+            </div>
+        </main>
+    }
+}
+
+#[component]
+fn Navbar() -> impl IntoView {
+    // Boring navigation bar
+    view! {
+        <Title text="Navigation bar"/>
+        <main>
+            <div class="navbar bg-base-100">
+                <div class="flex-none">
+                    <button class="btn btn-square btn-ghost">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-5 h-5 stroke-current"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex-1">
+                    // text
+                    <a class="btn btn-ghost text-xl">"Visual Start"</a>
+                </div>
+                <div class="flex-none">
+                    <button class="btn btn-square btn-ghost">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-5 h-5 stroke-current"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                            ></path>
+                        </svg>
                     </button>
                 </div>
             </div>
